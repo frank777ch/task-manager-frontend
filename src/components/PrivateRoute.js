c99c60ceb8b00351f4766ad4b0401ac0b7906dfc
IrisCode <150204523+frank777ch@users.noreply.github.com>
2024-07-08 14:40:07 -0500

    Update PrivateRoute.js
    Fixing not declarated prop "children" using prop-types.

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Navigate } from 'react-router-dom';
 
 const PrivateRoute = ({ children }) => {
   const isAuthenticated = !!localStorage.getItem('token');
   return isAuthenticated ? children : <Navigate to="/login" />;
+};
+
+PrivateRoute.propTypes = {
+  children: PropTypes.node.isRequired,
 };
 
 export default PrivateRoute;
